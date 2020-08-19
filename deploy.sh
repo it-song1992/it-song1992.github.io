6cc2eef74c788d41e7df203d094e94f049fb5003
it.song <it.song@ucloud.cn>
2020-08-19 16:01:41 +0800

    build: add git user config

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,9 @@ git commit -m 'github workflow ci deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+git config --local user.email "2625601001@qq.com"
+git config --local user.name "it-song1992"
 git push -f git@github.com:it-song1992/it-song1992.github.io.git master:gh-pages
 
 cd -

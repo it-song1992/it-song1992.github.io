fa1803856e3939853ed3ce6a26f820a31aced273
it.song <it.song@ucloud.cn>
2020-08-19 16:08:48 +0800

    docs: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,18 +12,27 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:it-song1992/it-song1992.github.io.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://it-song1992:${GITHUB_TOKEN}@github.com/it-song1992/it-song1992.github.io.git
+  git config --global user.name "it-song1992"
+  git config --global user.email "2625601001@qq.com"
+fi
+
 git init
 git add -A
-git commit -m 'github workflow ci deploy'
+git commit -m "${msg}"
+
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-git config --local user.email "2625601001@qq.com"
-git config --local user.name "it-song1992"
-git push -f git@github.com:it-song1992/it-song1992.github.io.git master:gh-pages
+git push -f $githubUrl master:gh-pages
 
 cd -
+rm -rf docs/.vuepress/dist
